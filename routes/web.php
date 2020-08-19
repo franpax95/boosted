adc3926310362e167043783fa7a3d4694e3bad78
Palumbo <fcojnavarrowork@gmail.com>
2020-08-19 12:14:59 +0200

    removed redirect https middleware

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{path?}', function() {
     return view('app');
-})->where('path', '.*')->middleware('redirectSecureHttp');
+})->where('path', '.*');
