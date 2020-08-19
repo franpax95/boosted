658489f18f5abd408252b19d6de4dd5f4a06cb61
Palumbo <fcojnavarrowork@gmail.com>
2020-08-19 12:36:11 +0200

    Añadida una ruta para ejecutar passport:install

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ use Illuminate\Support\Facades\Route;
 //     return view('app', ['user' => $user]);
 // } )->where('path', '.*');
 
+
+
 Route::get('/{path?}', function() {
-    return view('app');
+    \Artisan::call('passport:install');
+    //return view('app');
 })->where('path', '.*');
